8352ac30349876fbb5e45013c1ee8bd8177796a8
cagustavo11 <churaalaygustavo11@gmail.com>
2021-11-25 19:13:36 -0500

    I added brownboard

--- src/components/pages/home/ColorBlock.jsx
+++ src/components/pages/home/ColorBlock.jsx
@@ -24,7 +24,7 @@ const ColorBlock = ({ finalGrade, messageGrade, currentGrades }) => {
   }, [finalGrade]);
   return (
     <div>
-      {finalGrade !== 0 && (
+      {finalGrade !== 0 ? (
         <div
           className={`absolute top-0 bottom-0 left-0 right-0 rounded-lg ${asignColorBlock(
             finalGrade,
@@ -40,8 +40,10 @@ const ColorBlock = ({ finalGrade, messageGrade, currentGrades }) => {
               )}
 
               {error && (
-                <p className="font-semibold text-[20px] uppercase">
-                  Tus notas no pueden ser mayores a 20 o ser notas negativas
+                <p className="font-semibold text-[38px] uppercase">
+                  Tus notas no pueden ser mayores a{" "}
+                  <span className="text-red-900">20</span> o ser notas{" "}
+                  <span className="text-red-900">negativas</span>.
                 </p>
               )}
 
@@ -55,6 +57,19 @@ const ColorBlock = ({ finalGrade, messageGrade, currentGrades }) => {
                 {!error && messageGrade}
               </p>
             </span>
+          </div>
+        </div>
+      ) : (
+        <div
+          className={`absolute top-0 bottom-0 left-0 right-0 rounded-lg bg-[#D9735B]`}
+        >
+          <div className="flex items-center justify-center h-full">
+            <p className="font-semibold text-[40px] text-center">
+              <span className="block font-bold text-blue-900">
+                Bienvenid@ a Campromedio!!!
+              </span>
+              Lugar donde puedes calcular tus promedios de forma segura.
+            </p>
           </div>
         </div>
       )}
--- src/helpers/showGradeMessage.jsx
+++ src/helpers/showGradeMessage.jsx
@@ -1,14 +1,14 @@
 const showMessageGrade = (finalGrade) => {
   if (finalGrade >= 17) {
-    return "Tu nota es Excelente! 😆";
+    return "Tu nota es excelente, sigue así! 😆";
   } else if (finalGrade < 17 && finalGrade >= 14) {
-    return "Tu nota es buena! 😄";
+    return "Tu nota es buena!, pero aún puedes mejorar esa nota 😄";
   } else if (finalGrade < 14 && finalGrade >= 10.5) {
-    return "Tu nota es regular! 😃";
+    return "Tu nota es regular!, pero no te quedes ahí 😃";
   } else if (finalGrade < 10.5 && finalGrade >= 6) {
-    return "Tu nota es mala 😕";
+    return "Tu nota es mala, puedes mejorar 😕";
   } else if (finalGrade < 6 && finalGrade >= 0) {
-    return "Tu nota es muy mala 🙁";
+    return "Tu nota es muy mala, debes esforzarte más 🙁";
   }
 };
 
