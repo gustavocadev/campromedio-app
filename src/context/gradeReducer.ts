0689856e82fe32f82143f155fabc1e07572187d7
gustavocadev <gustavocadev@gmail.com>
2024-09-15 12:17:50 -0500

    Refactor gradeReducer to add SET_INITIAL_UNITS action type

--- src/context/gradeReducer.ts
+++ src/context/gradeReducer.ts
@@ -4,29 +4,39 @@ import type { GradeState } from './';
 type GradeActionType =
   | { type: 'SET_GRADES_BY_UNIT'; payload: Unit[] }
   | { type: 'SET_FINAL_GRADE'; payload: number }
-  | { type: 'SET_MESSAGE_GRADE'; payload: string };
+  | { type: 'SET_MESSAGE_GRADE'; payload: string }
+  | { type: 'SET_INITIAL_UNITS'; payload: Unit[] };
 
-const gradeReducer = (state: GradeState, action: GradeActionType) => {
+export const gradeReducer = (state: GradeState, action: GradeActionType) => {
   switch (action.type) {
-    case 'SET_MESSAGE_GRADE':
+    case 'SET_MESSAGE_GRADE': {
       return {
         ...state,
         messageGrade: action.payload,
       };
-    case 'SET_GRADES_BY_UNIT':
+    }
+    case 'SET_INITIAL_UNITS': {
       return {
         ...state,
         units: action.payload,
       };
+    }
 
-    case 'SET_FINAL_GRADE':
+    case 'SET_GRADES_BY_UNIT': {
+      return {
+        ...state,
+        units: action.payload,
+      };
+    }
+
+    case 'SET_FINAL_GRADE': {
       return {
         ...state,
         finalGrade: action.payload,
       };
+    }
 
     default:
       return state;
   }
 };
-export { gradeReducer };
