1a4ae75675ed41ee44085228de67c0b3aafa03ed
gustavocadev <gustavocadev@gmail.com>
2024-09-15 16:35:43 -0500

    Refactor App component to add ThemeProvider for managing themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
-import { Route, Routes } from "react-router-dom";
-import { GradeProvider } from "./context";
-import Layout from "./layouts/Layout";
-import Home from "./routes/home/Home";
-import Contributes from "./routes/contribute/Contributes";
+import { Route, Routes } from 'react-router-dom';
+import { GradeProvider } from './context';
+import Layout from './layouts/Layout';
+import Home from './routes/home/Home';
+import Contributes from './routes/contribute/Contributes';
+import { ThemeProvider } from './components/theme-provider';
 
 const App = () => {
   return (
-    <GradeProvider>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="contributes" element={<Contributes />} />
-        </Route>
-      </Routes>
-    </GradeProvider>
+    <ThemeProvider>
+      <GradeProvider>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="contributes" element={<Contributes />} />
+          </Route>
+        </Routes>
+      </GradeProvider>
+    </ThemeProvider>
   );
 };
 
