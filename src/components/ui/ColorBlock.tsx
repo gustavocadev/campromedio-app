8a114267474fea4765ea46e551352fd52207e18f
gustavocadev <gustavocadev@gmail.com>
2024-09-15 17:46:21 -0500

    Refactor ColorBlock component for responsive visibility and styling

--- src/components/ui/ColorBlock.tsx
+++ src/components/ui/ColorBlock.tsx
@@ -24,37 +24,37 @@ const ColorBlock = () => {
           )}`}
         >
           <div className="flex items-center justify-center h-full">
-            <span className="text-center">
+            <span className="text-center leading-relaxed space-y-2 p-4">
               {!error && (
-                <p className="font-semibold text-[40px] uppercase">
+                <p className="font-semibold text-4xl lg:text-5xl uppercase">
                   {finalGrade >= 10.5 ? '¡Aprobaste!' : '¡REPROBASTE!'}
                 </p>
               )}
 
               {error && (
-                <p className="font-semibold text-[38px] uppercase">
+                <p className="font-semibold lg:text-3xl uppercase">
                   Tus notas no pueden ser mayores a{' '}
                   <span className="text-red-900">20</span> o ser notas{' '}
                   <span className="text-red-900">negativas</span>.
                 </p>
               )}
 
-              <p className="font-semibold text-[18px]">
+              <p className="font-semibold text-base lg:text-xl">
                 {!error && 'Tu nota final es:'}
               </p>
-              <p className="font-semibold text-[64px]">
-                {!error && finalGrade.toFixed(1)}
+              <p className="font-semibold text-5xl lg:text-6xl">
+                {!error && finalGrade.toFixed(2)}
               </p>
-              <p className="font-semibold text-[22px]">
+              <p className="font-semibold text-lg lg:text-xl">
                 {!error && messageGrade}
               </p>
             </span>
           </div>
         </div>
       ) : (
-        <div className="absolute top-0 bottom-0 left-0 right-0 rounded-lg bg-[#D9735B] min-h-[500px] lg:h-auto">
+        <div className="absolute top-0 bottom-0 left-0 right-0 rounded-lg bg-[#D9735B] min-h-64 lg:h-auto">
           <div className="flex items-center justify-center h-full">
-            <p className="font-semibold text-[40px] text-center">
+            <p className="font-semibold text-2xl lg:text-[40px] text-center leading-relaxed">
               <span className="block font-bold text-blue-900">
                 ¡Bienvenid@ a Campromedio!
               </span>
