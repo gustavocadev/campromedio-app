202bdc26aed94d49e525a2169dadb18ba0150c52
gustavocadev <gustavocadev@gmail.com>
2024-09-15 11:45:34 -0500

    Refactor Footer component to remove unused code and update copyright year

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,42 +1,13 @@
-import { useLocation } from "react-router-dom"
-// import contactoBuho from "../../assets/img/contactoBuho.png"
-
 const Footer = () => {
-  const { pathname } = useLocation()
-  // console.log(pathname)
   return (
     <footer className="mt-[10px] ">
       <header className="flex justify-center">
         <h3 className="py-[24px] text-[16px] font-semibold text-center">
-          CAMPROMEDIO &copy; 2021
+          CAMPROMEDIO &copy; 2021 - {new Date().getFullYear()}
         </h3>
       </header>
-
-      <figure className="flex justify-end mt-[-94px] mr-[-80px] relative z-50">
-        {/* {pathname === "/" ? (
-          <img
-            src={homeBuho}
-            alt="Buho con varita magica"
-            className="w-[233px] mt-[-208px]"
-          />
-        ) : (
-          <img
-            src={contactoBuho}
-            alt="Buho con su celular"
-            className="w-[200px] mt-[-138px]"
-          />
-        )} */}
-
-        {pathname === "/" && (
-          <img
-            src="/img/homeBuho.png"
-            alt="Buho con varita magica"
-            className="w-[233px] mt-[-208px]"
-          />
-        )}
-      </figure>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
